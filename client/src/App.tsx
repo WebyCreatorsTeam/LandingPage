e38c3276f3e96bf07122b2d3a1146ae88671882c
ESTIPAPPENHAIM <grosberg002@gmail.com>
2024-01-03 00:10:18 +0200

    style

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,9 @@
-import './App.css';
 import UserForm from './View/Components/Form/Form';
 import Header from './View/Components/Header/Header';
 import HowWorks from './View/Components/HowWorks/HowWorks';
 import Offer from './View/Components/Offer/Offer';
 import ContactInfo from './View/UI/ContactInfo/ContactInfo';
+import './style/index.css'
 
 function App() {
   return (
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import RouterPage from './Router/RouterPage';
 
 const root = ReactDOM.createRoot(
