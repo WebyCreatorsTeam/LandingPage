52b28bc77296cf0ccb2c653706b3d7d9a059eded
ESTIPAPPENHAIM <grosberg002@gmail.com>
2024-01-03 20:02:05 +0200

    style

--- client/src/View/UI/ContactInfo/ContactInfo.tsx
+++ client/src/View/UI/ContactInfo/ContactInfo.tsx
@@ -5,14 +5,15 @@ import IconEmail from '../../../images/contact/icon-mail.png'
 const ContactInfo: FC = () => {
     return (
         <div className='info-container'> {/* display: flex */} {/*, חשבתי לשים את האלמנטים בפלקס על מנת שיהיו אחד ליד השני */}
+
+        <div className='info-icon-container'> {/* display: flex */} {/*, חשבתי לשים את האלמנטים בפלקס על מנת שיהיו אחד ליד השני */}
+        <a href="tel:972547991963" className='info-text'>054-799-1963</a>
+        <img className='info-icon' src={IconPhone} alt="אייקון של טלפון" />
+    </div>
+
             <div className='info-icon-container'>
                 <a href="mailto:webycreatorsteam@gmail.com" className='info-text'>webycreatorsteam@gmail.com</a>
                 <img className='info-icon' src={IconEmail} alt="אייקון של מכתב" />
-            </div>
-
-            <div className='info-icon-container'> {/* display: flex */} {/*, חשבתי לשים את האלמנטים בפלקס על מנת שיהיו אחד ליד השני */}
-                <a href="tel:972547991963" className='info-text'>054-799-1963</a>
-                <img className='info-icon' src={IconPhone} alt="אייקון של טלפון" />
             </div>
         </div>
     )
