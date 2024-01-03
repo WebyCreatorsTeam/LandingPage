07ac2c0c2e14e0816cffea57014aed1ea637b261
Katya Rukosuev <72752681+RuKatya@users.noreply.github.com>
2024-01-03 23:14:05 +0200

    logo

--- client/src/View/Components/WhyUs/WhyUs.tsx
+++ client/src/View/Components/WhyUs/WhyUs.tsx
@@ -1,16 +1,25 @@
-import { FC } from 'react'
-import Logo from '../../UI/Logo/Logo'
+import { FC } from "react";
+import Logo_Big from "../../../images/logo-big.png";
 
 const WhyUs: FC = () => {
     return (
         <div>
             <h1>למה לבחור דווקא בנו?</h1>
-            <div> {/* display: flex */} {/*חשבתי לשים את האלמנטים בפלקס על מנת שיהיו אחד ליד השני */}
-                <p>כחברה חדשה, המחויבות שלנו ברורה - אנו מציעים את שירותינו המצוינים בחינם שבו כל אחד מאיתנו מומחה בתחום אחר.</p>
-                <Logo />
+            <div>
+                {" "}
+                {/* display: flex */}{" "}
+                {/*חשבתי לשים את האלמנטים בפלקס על מנת שיהיו אחד ליד השני */}
+                <p>
+                    כחברה חדשה, המחויבות שלנו ברורה - אנו מציעים את שירותינו
+                    המצוינים בחינם שבו כל אחד מאיתנו מומחה בתחום אחר.
+                </p>
+                <img
+                    src={Logo_Big}
+                    alt="Big Weby Logo"
+                />
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default WhyUs
+export default WhyUs;
