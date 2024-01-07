aa5e0a102f01eacf202799ae0d7c221cd8669331
Katya Rukosuev <72752681+RuKatya@users.noreply.github.com>
2024-01-07 23:21:28 +0200

    Update dashboard.controller.js

--- server/controllers/dashboard/dashboard.controller.js
+++ server/controllers/dashboard/dashboard.controller.js
@@ -1,59 +1,17 @@
 const { User } = require("../../model/user.model");
 const { httpCodes } = require("../../utils/httpStatusCode");
-// const { userValidation } = require("../../utils/validation/user.validation");
 
 exports.getDetails = async (req, res) => {
-
     try {
         const allUsers = await User.find({})
         console.log(allUsers)
 
-        return res.send("get")
-        // console.log(req.body);
-
-        // const {
-        //     inputFields: { userName, userEmail, userPhone, userHelp },
-        // } = req.body;
-        // console.log(userName, userEmail, userPhone, userHelp);
-
-        // const { error } = userValidation.validate({
-        //     userName,
-        //     userEmail,
-        //     userPhone,
-        //     userHelp,
-        // });
-
-        // if (error) {
-        //     console.error(
-        //         "user validation error of sendDetails: ",
-        //         error.message
-        //     );
-        //     return (
-        //         res
-        //             // .status(httpCodes.FORBIDDEN)
-        //             .send({ continueWork: false, message: error.message })
-        //     );
-        // }
-
-        // const newDetails = new User({
-        //     userName,
-        //     userEmail,
-        //     userPhone,
-        //     userHelp,
-        // });
-
-        // await newDetails.save();
-
-        // return res.status(httpCodes.OK).send({
-        //     continueWork: true,
-        //     message: "קיבלנו את הפרטים שלך, נחזור בהקדם",
-        // });
+        return res.status(httpCodes.OK).send(allUsers);
     } catch (error) {
         console.error(error);
         return res.send({
             continueWork: false,
             message: "שגיאה בשרת, נסה שנית",
         });
-        // .status(httpCodes.SERVER_ERROR);
     }
 };
