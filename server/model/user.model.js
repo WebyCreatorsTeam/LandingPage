cadcffe84392892b0d19b9b3155a0beed6b8fc9d
Katya Rukosuev <72752681+RuKatya@users.noreply.github.com>
2024-02-18 01:15:27 +0200

    Update user.model.js

--- server/model/user.model.js
+++ server/model/user.model.js
@@ -4,7 +4,9 @@ const UserSchema = new Schema({
     userName: { type: String, require: true },
     userEmail: { type: String, require: true, trim: true, lowercase: true },
     userPhone: { type: String, require: true },
-    userHelp: { type: String, require: true }
+    userHelp: { type: String, require: true },
+    archive: {type: Boolean, default: false},
+    favorite: {type: Boolean, default: false}
 })
 
 exports.User = model("User", UserSchema)
