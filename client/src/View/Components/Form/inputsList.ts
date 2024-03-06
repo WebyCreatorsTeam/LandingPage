c423c34408b02db299a1fcdf13fb04ac73cb1854
Katya Rukosuev <72752681+RuKatya@users.noreply.github.com>
2024-03-06 14:32:37 +0200

    Update inputsList.ts

--- client/src/View/Components/Form/inputsList.ts
+++ client/src/View/Components/Form/inputsList.ts
@@ -3,7 +3,7 @@ import { InputsList, OptionsList } from "./types";
 export const inputs: Array<InputsList> = [
     { type: "text", name: "userName", placeholder: "שם מלא/חברה*" },
     { type: "text", name: "userEmail", placeholder: "אימייל*" },
-    { type: "text", name: "userPhone", placeholder: "מספר טלפון*"},
+    { type: "text", name: "userPhone", placeholder: "מספר טלפון*" },
 ];
 
 export const options: Array<OptionsList> = [
@@ -11,6 +11,5 @@ export const options: Array<OptionsList> = [
     { value: "dev", text: "פיתוח אתר" },
     { value: "uxui", text: "UX/UI ופיתוח אתר" },
     { value: "support", text: "תחזוקה של אתר" },
-    { value: "vebinar", text: "הרשמה לובינר 19.03.2024" },
     { value: "other", text: "אחר" },
 ];
