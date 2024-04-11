7f60d82853225a0bb78df44f47c9aaf188ff037f
Katya Rukosuev <72752681+RuKatya@users.noreply.github.com>
2024-04-11 15:35:42 +0300

    Update user.controller.js

--- server/controllers/users/user.controller.js
+++ server/controllers/users/user.controller.js
@@ -22,9 +22,11 @@ exports.sendDetails = async (req, res) => {
 
         transporter.sendMail(mailOptions(userName, userEmail, userPhone, userHelp), (error, info) => {
             if (error) {
+                console.log(`email transporter enter error`)
                 console.error("Error sending email: ", error);
             } else {
                 console.log("Email sent: ", info.response);
+                console.log(`email transporter ente sentr`)
             }
         });
 
