e5d64c70bfc56b6c09c533fc718dc4cf20f6e08e
Katya Rukosuev <72752681+RuKatya@users.noreply.github.com>
2024-04-29 23:53:47 +0300

    webp images

--- client/src/View/Components/Components/Offer/offerArray.ts
+++ client/src/View/Components/Components/Offer/offerArray.ts
@@ -1,6 +1,6 @@
-import Landing from '../../../../images/offer/image 5.png'
-import Corporate from '../../../../images/offer/image 6.png'
-import Commerce from '../../../../images/offer/image 7.png'
+import Landing from '../../../../images/offer/image 5.webp'
+import Corporate from '../../../../images/offer/image 6.webp'
+import Commerce from '../../../../images/offer/image 7.webp'
 
 export const offerArray: Array<IOffer> = [
     { img: Landing, siteTypeHe: "דף נחיתה", siteTypeEn: "Landing Page", siteDesc: "עמוד אינטרנט שמיועד למשוך משתמשים לפעולה מסוימת, כגון רכישת מוצר, מילוי טופס וכד'." },
@@ -14,8 +14,3 @@ interface IOffer {
     siteTypeEn: string
     siteDesc: string
 }
-
-
-/*
-A visually stunning and user-friendly e-commerce website design, with a modern and clean layout that invites exploration. The large, bold headline captures attention and is accompanied by a selection of enticing products. The muted color palette seamlessly blends with the light background, creating a harmonious and cohesive visual experience. The overall design exudes sophistication, ensuring a pleasant shopping experience for visitors.
-*/
