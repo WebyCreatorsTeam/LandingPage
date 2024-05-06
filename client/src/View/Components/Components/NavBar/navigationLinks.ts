9753e208c1184a0fad53a76a2dfaa1e8711da659
Katya Rukosuev <72752681+RuKatya@users.noreply.github.com>
2024-05-06 17:05:23 +0300

    Adding FAQ Section

--- client/src/View/Components/Components/NavBar/navigationLinks.ts
+++ client/src/View/Components/Components/NavBar/navigationLinks.ts
@@ -4,5 +4,5 @@ export const navigationLinks: Array<Navigation> = [
     { link: "about", title: "מי אנחנו" },
     { link: "pojects", title: "פרויקטים" },
     { link: "feedbacks", title: "פידבקים" },
-    // { link: "faq", title: "שאלות" },
+    { link: "faq", title: "שאלות נפוצות" },
 ]
--- client/src/View/Pages/HomePage.tsx
+++ client/src/View/Pages/HomePage.tsx
@@ -5,8 +5,8 @@ import Offer from '../Components/Components/Offer/Offer'
 import HowWork from '../Components/Components/HowWork'
 import Projects from '../Components/Components/Projects/Projects'
 import Feedbacks from '../Components/Components/Feedbacks'
-// import FAQ from '../Components/Components/FAQ'
 import UserForm from '../Components/Components/UserForm'
+import FAQ from '../Components/Components/FAQ/FAQ'
 
 const HomePage: FC = () => {
     return (
@@ -21,11 +21,9 @@ const HomePage: FC = () => {
                 <Projects />
                 <div className='bottom-elements-bkg'>
                     <Feedbacks />
+                    <FAQ />
                     <UserForm />
                 </div>
-                {/* 
-                <FAQ />
-                */}
             </main>
         </>
     )
