f34e390a2a9e13ec252d887ff67d68902f7a1f59
Katya Rukosuev <72752681+RuKatya@users.noreply.github.com>
2024-05-09 00:58:53 +0300

    Update SEO.tsx

--- client/src/SEO/SEO.tsx
+++ client/src/SEO/SEO.tsx
@@ -30,7 +30,7 @@ const SEO: FC<ISEO> = ({
             <meta name="description"
                 content={description ? description : "שירותי פיתוח אתרים מתקדמים. מומחים בבניית אתרים מותאמים אישית עבור העסק, מספקים פתרונות דיגיטליים מקצה לקצה ברמה הגבוהה ביותר."} />
             <meta property="og:type" content="website" />
-            <meta property="og:image" content={image ? `https://weby.team/${image}` : "https://weby.team/logo_white_bkgr.png"} />
+            <meta property="og:image" content={image ? image : "https://weby.team/logo_white_bkgr.png"} />
             <meta property="og:image:alt" content={imageAlt ? imageAlt : "לוגו של וובי"} />
             <meta property="og:url" content={url ? url : "https://weby.team"} />
             <meta property="og:title" content={title ? title : "Weby"} />
