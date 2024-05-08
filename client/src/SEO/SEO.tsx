70057e9a9f44c35be1bab98817eda79de9c8fe02
Katya Rukosuev <72752681+RuKatya@users.noreply.github.com>
2024-05-09 00:54:56 +0300

    seo

--- client/src/SEO/SEO.tsx
+++ client/src/SEO/SEO.tsx
@@ -4,6 +4,7 @@ import { Helmet } from 'react-helmet-async';
 export interface ISEO {
     title?: string,
     description?: string
+    keywords?: string
     image?: string,
     imageAlt?: string
     url?: string,
@@ -14,6 +15,7 @@ export interface ISEO {
 const SEO: FC<ISEO> = ({
     title,
     description,
+    keywords,
     image,
     imageAlt,
     url,
@@ -24,8 +26,11 @@ const SEO: FC<ISEO> = ({
     return (
         <Helmet>
             <title>{title ? `${title}` : "Weby"}</title>
+            <meta name="keywords" content={keywords ? keywords : "פיתוח אתר, עיצוב אתר, בניית אתרים, אתר לעסק, פתרונות דיגיטליים, בית תוכנה"} />
+            <meta name="description"
+                content={description ? description : "שירותי פיתוח אתרים מתקדמים. מומחים בבניית אתרים מותאמים אישית עבור העסק, מספקים פתרונות דיגיטליים מקצה לקצה ברמה הגבוהה ביותר."} />
             <meta property="og:type" content="website" />
-            <meta property="og:image" content={image ? image : "https://weby.team/logo_white_bkgr.png"} />
+            <meta property="og:image" content={image ? `https://weby.team/${image}` : "https://weby.team/logo_white_bkgr.png"} />
             <meta property="og:image:alt" content={imageAlt ? imageAlt : "לוגו של וובי"} />
             <meta property="og:url" content={url ? url : "https://weby.team"} />
             <meta property="og:title" content={title ? title : "Weby"} />
